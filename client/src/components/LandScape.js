6f220be2666a3fbd5b73a1755bf6aa5d63692a73
t-r-griffin <westonroadflows@gmail.com>
2020-06-26 07:29:13 +0100

    yay

--- client/src/components/LandScape.js
+++ client/src/components/LandScape.js
@@ -1,5 +1,14 @@
 import React, { Fragment } from 'react';
 
 export default function LandScape() {
-  return <div className="LandScape">You can't sit here</div>;
+  return (
+    <div className="LandScape">
+      <div className="flexo ">
+        <i className="large material-icons phone" style={{ fontSize: '200px' }}>
+          phone_iphone
+        </i>
+        <div className="below">Please rotate your device to portrait</div>
+      </div>
+    </div>
+  );
 }
