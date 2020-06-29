f1bb4c44055ea0508a8870ec4761cb44aa75c7f3
t-r-griffin <westonroadflows@gmail.com>
2020-06-29 10:45:49 +0100

    jsjdk

---
+++ client/src/components/Img.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Observer from 'react-intersection-observer';
+export default function Img(props) {
+  const { src, width, height, ...rest } = props;
+
+  return (
+    <div className="root" style={{ '--maxWidth': width }}>
+      <Observer triggerOnce={true}>
+        {({ inView, ref }) => (
+          <div
+            ref={ref}
+            className="wrapper"
+            style={{
+              '--height': height,
+              '--width': width,
+            }}
+          >
+            <img
+              src={inView ? src : ''}
+              width={width}
+              height={height}
+              className="img"
+              {...rest}
+            />
+          </div>
+        )}
+      </Observer>
+    </div>
+  );
+}
--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -1,40 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import cards from '../cards.png';
+import Img from './Img';
 
 const Menu = () => {
-  const [image, setImage] = useState(false);
+  return (
+    <div className="menu">
+      <h1 className="moveInLeft">BlackJack</h1>
 
-  useEffect(() => {
-    const img = new Image();
-    img.src = cards;
-
-    img.onload = () => {
-      setImage(true);
-    };
-  });
-  if (!image) {
-    return <div>wtf</div>;
-  } else {
-    return (
-      <div className="menu">
-        <h1 className="moveInLeft">BlackJack</h1>
-
-        <img className="menu__card fadeIn" src={cards} alt="Cards" />
-        <div className="options fadeIn">
-          <Link to="/Play" className="btn">
-            Play
-          </Link>
-          <Link to="/Rules" className="btn">
-            Rules
-          </Link>
-          <Link to="/Settings" className="btn">
-            Settings
-          </Link>
-        </div>
+      <Img
+        width={202}
+        height={204}
+        className="menu__card fadeIn"
+        src={cards}
+        alt="Cards"
+      />
+      <div className="options fadeIn">
+        <Link to="/Play" className="btn">
+          Play
+        </Link>
+        <Link to="/Rules" className="btn">
+          Rules
+        </Link>
+        <Link to="/Settings" className="btn">
+          Settings
+        </Link>
       </div>
-    );
-  }
+    </div>
+  );
 };
 
 export default Menu;
