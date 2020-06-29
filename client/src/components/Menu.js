72fce810a80a1ad4e359863791a98ebd7184ea9f
t-r-griffin <westonroadflows@gmail.com>
2020-06-29 04:14:32 +0100

    fixed

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import cards from '../cards.png';
 
 const Menu = () => {
-  return (
-    <div className="menu">
-      <h1 className="moveInLeft">BlackJack</h1>
+  const [image, setImage] = useState(false);
 
-      <img className="menu__card fadeIn" src={cards} alt="Cards" />
-      <div className="options fadeIn">
-        <Link to="/Play" className="btn">
-          Play
-        </Link>
-        <Link to="/Rules" className="btn">
-          Rules
-        </Link>
-        <Link to="/Settings" className="btn">
-          Settings
-        </Link>
+  useEffect(() => {
+    const img = new Image();
+    img.src = cards;
+
+    img.onload = () => {
+      setImage(true);
+    };
+  });
+  if (!image) {
+    return null;
+  } else {
+    return (
+      <div className="menu">
+        <h1 className="moveInLeft">BlackJack</h1>
+
+        <img className="menu__card fadeIn" src={cards} alt="Cards" />
+        <div className="options fadeIn">
+          <Link to="/Play" className="btn">
+            Play
+          </Link>
+          <Link to="/Rules" className="btn">
+            Rules
+          </Link>
+          <Link to="/Settings" className="btn">
+            Settings
+          </Link>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default Menu;
