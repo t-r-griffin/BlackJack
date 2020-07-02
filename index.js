352b0a4bcbd19871a94d547e52c2e3d2eb401205
t-r-griffin <westonroadflows@gmail.com>
2020-07-02 02:03:08 +0100

    jajksjs

--- index.js
+++ index.js
@@ -26,5 +26,5 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT);
