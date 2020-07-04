c229bc3f21301a2c9b8929a5a3005ddf7b1b5ece
t-r-griffin <westonroadflows@gmail.com>
2020-07-04 16:46:29 +0100

    aaaaaaaaaa

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ const http = require('http');
 const enforce = require('express-sslify');
 const fs = require('fs');
 
-const key = fs.readFileSync('./key.pem');
-const cert = fs.readFileSync('./cert.pem');
+const key = fs.readFileSync('./config/key.pem');
+const cert = fs.readFileSync('./config/cert.pem');
 
 const app = express();
 
 const server = http.createServer({ key: key, cert: cert }, app);
 
-//app.use(enforce.HTTPS({ trustProtoHeader: true }));
+if (process.env.NODE_ENV === 'production') {
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
+}
 
 app.use(express.static('client/build'));
 
